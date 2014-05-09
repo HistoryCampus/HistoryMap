5f59a756333fb3dec28fe11ea10c7b2b2132933b
Norman Köhring <n@koehr.in>
2014-05-09 17:50:31 +0200

    first approach to calculate latLng→XY, failed

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ $(function() {
   this.mapHeight = this.map.height();
   this.mapWidth = this.map.width();
 
+  this.mapLatLngBBox = [
+    {lat: 60.9518, lng: -14.8535},
+    {lat: 35.1379, lng: 42.4512}
+  ];
+
+  d = this.mapLatLngBBox[1].lng - this.mapLatLngBBox[0].lng;
+  this.mapPixelFactor = this.mapWidth / d;
+
   this.markerClass = "marker";
   this.markerPrototype = $('<img src="marker.svg" class="marker" width="40px">');
   this.markerLayer = $('#marker-layer');
@@ -15,15 +23,18 @@ $(function() {
     self.map.height($(document).height() * 0.9);
     self.mapHeight = self.map.height();
     self.mapWidth = self.map.width();
+
+    d = self.mapLatLngBBox[1].lng - self.mapLatLngBBox[0].lng;
+    self.mapPixelFactor = self.mapWidth / d;
   };
 
-  this.placeMarker = function(x,y) {
+  this.placeMarker = function(pos) {
     var marker = self.markerPrototype.clone(),
         markerWidth = marker.width(),
         markerHeight = marker.height();
 
-    x = self.mapBBox.left + x - markerWidth / 2;
-    y = self.mapBBox.top + y - markerHeight;
+    x = self.mapBBox.left + pos.x - markerWidth / 2;
+    y = self.mapBBox.top + pos.y - markerHeight;
 
     marker.offset({ top: y, left: x });
 
@@ -36,11 +47,19 @@ $(function() {
     self.markerLayer.empty();
   };
 
+  this.latLngToXY = function(lat, lng) {
+    var pos = {
+      x: lat * self.mapPixelFactor,
+      y: lng * self.mapPixelFactor
+    };
+    return pos;
+  };
+
   $(window).resize(this.update);
   this.update();
-  this.placeMarker(100,100);
-  this.placeMarker(200,300);
-  this.placeMarker(300,200);
+
+  //DEBUG
+  this.placeMarker(this.latLngToXY(52.5, 13.4));
 
 });
 
