efceea99b446616863818562de9fa9e3ebad0369
Norman Köhring <n@koehr.in>
2014-05-10 02:57:44 +0200

    fancy math
    The first thing I said after that idea of calculating lat/lng to pixels was
    something like: That could be hard, maybe consider use a mapping service
    instead. But we made it without that. That means, I made it.
    
    Now it's 3am.

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
+if (typeof Number.prototype.toRadians == 'undefined') {
+    Number.prototype.toRadians = function() {
+        return this * Math.PI / 180;
+    }
+}
+
 $(function() {
 
   self = this;
-  this.map = $('#map');
-  this.mapBBox = this.map.offset();
-  this.mapHeight = this.map.height();
-  this.mapWidth = this.map.width();
+
+  map = document.getElementById('map-contours');
+  this.mapBBox = map.getBoundingClientRect();
 
   this.mapLatLngBBox = [
     {lat: 60.9518, lng: -14.8535},
     {lat: 35.1379, lng: 42.4512}
   ];
 
-  d = this.mapLatLngBBox[1].lng - this.mapLatLngBBox[0].lng;
-  this.mapPixelFactor = this.mapWidth / d;
+  this.markerPrototype = $('#marker-prototype');
+  this.markerSize = {
+    w: this.markerPrototype.width(),
+    h: this.markerPrototype.height()
+  }
 
-  this.markerClass = "marker";
-  this.markerPrototype = $('<img src="marker.svg" class="marker" width="40px">');
   this.markerLayer = $('#marker-layer');
   this.markers = [];
 
   this.update = function() {
-    self.mapHeight = self.map.height();
-    self.mapWidth = self.map.width();
-
-    d = self.mapLatLngBBox[1].lng - self.mapLatLngBBox[0].lng;
-    self.mapPixelFactor = self.mapWidth / d;
   };
 
   this.placeMarker = function(pos) {
     var marker = self.markerPrototype.clone(),
-        markerWidth = marker.width(),
-        markerHeight = marker.height();
-
-    x = self.mapBBox.left + pos.x - markerWidth / 2;
-    y = self.mapBBox.top + pos.y - markerHeight;
-
-    marker.offset({ top: y, left: x });
+        w = self.markerSize.w,
+        h = self.markerSize.h;
+    marker.removeAttr("id");
 
     self.markers.push(marker);
     self.markerLayer.append(marker);
+
+    //x = self.mapBBox.left + pos.x - w / 2;
+    //y = self.mapBBox.top + pos.y - h;
+    dx = self.mapBBox.left - w/2;
+    dy = self.mapBBox.top - h*2;
+    x = pos.x + dx;
+    y = pos.y + dy;
+
+    marker.offset({ top: y, left: x });
   };
 
   this.clearAllMarkers = function() {
@@ -47,18 +53,59 @@ $(function() {
   };
 
   this.latLngToXY = function(lat, lng) {
-    var pos = {
-      x: lat * self.mapPixelFactor,
-      y: lng * self.mapPixelFactor
+    var dy, dx, verticalFactor, pos;
+
+    xKmPerPx = self.mapWidthAtLat(lat) / self.mapBBox.width;
+    yKmPerPx = self.mapHeightAtLng(lng) / self.mapBBox.width;
+
+    dy = self.sphericalDistance(
+      {lat: self.mapLatLngBBox[0].lat, lng: lng},
+      {lat: lat, lng: lng}
+    );
+    dx = self.sphericalDistance(
+      {lat: lat, lng: self.mapLatLngBBox[0].lng},
+      {lat: lat, lng: lng}
+    );
+
+    pos = {
+      x: dx / xKmPerPx,
+      y: dy / yKmPerPx
     };
+
     return pos;
+  };
+
+  this.sphericalDistance = function(from, to) {
+    var φ1 = from.lat.toRadians(),
+        φ2 = to.lat.toRadians(),
+        Δλ = (to.lng - from.lng).toRadians(),
+        R = 6378.137; // gives d in km
+
+    return Math.acos( Math.sin(φ1)*Math.sin(φ2) + Math.cos(φ1)*Math.cos(φ2) * Math.cos(Δλ) ) * R;
+  };
+
+  this.mapWidthAtLat = function(lat) {
+    return self.sphericalDistance(
+      {lat: lat, lng: self.mapLatLngBBox[0].lng},
+      {lat: lat, lng: self.mapLatLngBBox[1].lng}
+    );
+  };
+
+  this.mapHeightAtLng = function(lng) {
+    return self.sphericalDistance(
+      {lat: self.mapLatLngBBox[0].lat, lng: lng},
+      {lat: self.mapLatLngBBox[1].lat, lng: lng}
+    );
   };
 
   $(window).resize(this.update);
   this.update();
 
   //DEBUG
-  this.placeMarker(this.latLngToXY(52.5, 13.4));
+  //this.placeMarker(this.latLngToXY(35.1379, 42.4512));
+  this.placeMarker(this.latLngToXY(52.5,13.5));
+  this.placeMarker(this.latLngToXY(51.5, 0));
+    
 
 });
 
